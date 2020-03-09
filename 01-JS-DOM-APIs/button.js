d97bc991dc998c312ed6c224ce82195be496abbc
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-03-09 19:13:41 -0300

    exercise 3, fetch api and show in html section

--- 01-JS-DOM-APIs/button.js
+++ 01-JS-DOM-APIs/button.js
@@ -4,4 +4,18 @@ function showAlert() {
     alert("Alert button clicked!");
 }
 
-button.addEventListener("click", showAlert);
+//button.addEventListener("click", showAlert);
+
+button.onclick = function() {
+    const api = "http://api.icndb.com/jokes/random";
+    fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            let section = document.getElementById("section");
+
+            section.innerHTML = `<h1>Joke Random</h1> 
+                            </br> 
+                            <p>${data.value.joke}</p>`;
+        })
+        .catch(err => console.log(err));
+};
