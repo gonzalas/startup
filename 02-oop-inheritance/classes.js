d934ca7f0c160736a86e90c45b769ce61deae27c
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-03-16 12:08:39 -0300

    creating an EventEmitter class

--- 02-oop-inheritance/classes.js
+++ 02-oop-inheritance/classes.js
@@ -43,3 +43,31 @@ class Actor {
         this.age = age;
     }
 }
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(eventName, callback) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = callback;
+        }
+    }
+
+    emit(eventName) {
+        if (!this.events[eventName]) {
+            console.log("No event to emit.");
+            return;
+        }
+        this.events[eventName] = e => {
+            console.log("Emit " + e);
+        };
+    }
+
+    off(eventName) {
+        if (this.events[eventName]) {
+            delete this.events[eventName];
+        }
+    }
+}
