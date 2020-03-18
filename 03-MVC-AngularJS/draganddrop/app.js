7dfd661c050dab84e26df53a60c4f5a585c53c49
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-03-18 15:01:35 -0300

    drag and drop correction, adding request funcionality

--- 03-MVC-AngularJS/draganddrop/app.js
+++ 03-MVC-AngularJS/draganddrop/app.js
@@ -1,5 +1,6 @@
 const startText = document.getElementById('start-text');
 const finishText = document.getElementById('finish-text');
+const dropFile = document.getElementById('drop-file');
 
 startText.addEventListener('dragstart', e => {
     e.dataTransfer.setData('text/plain', e.target.id);
@@ -15,3 +16,26 @@ finishText.addEventListener('drop', e => {
     element.classList.remove('active');
     finishText.appendChild(startText.removeChild(element));
 });
+
+//***************************/
+
+dropFile.addEventListener('dragover', e => {
+    e.preventDefault();
+});
+
+dropFile.addEventListener('drop', e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let files = e.dataTransfer.files;
+    if (files[0].type === 'text/plain') {
+        const reader = new FileReader();
+        reader.readAsText(files[0]);
+        reader.onload = fileRecieved();
+    }
+});
+
+function fileRecieved() {
+    const text = document.getElementById('drop-text');
+    text.innerHTML = 'File .txt recieved';
+}
