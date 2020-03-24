efdd158291072b816b60fffb66c0529a77bb3323
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-03-24 19:38:52 -0300

    Creating movie detail view, configuring routes

--- 06-Angular/movie-listing/src/app/app.module.ts
+++ 06-Angular/movie-listing/src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, MovieListComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    MovieListComponent,
+    MovieDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "", component: MovieListComponent },
+      { path: "movies/:movieId", component: MovieDetailComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ 06-Angular/movie-listing/src/app/movie-detail/movie-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { movies } from "../movies";
+
+@Component({
+  selector: "app-movie-detail",
+  templateUrl: "./movie-detail.component.html",
+  styleUrls: ["./movie-detail.component.css"]
+})
+export class MovieDetailComponent implements OnInit {
+  movie;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.movie = movies[+params.get("movieId")];
+    });
+  }
+}
